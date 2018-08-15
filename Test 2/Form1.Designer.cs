3368f8370955e2d7257d9fe78eabd6309fbf767a
Eric Mailman <emailman@dtcc.edu>
2018-08-15 15:30:38 -0400

    Add project files.

---
+++ Test 2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Test_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height";
+            // 
+            // tbxHeight
+            // 
+            this.tbxHeight.Location = new System.Drawing.Point(75, 13);
+            this.tbxHeight.Name = "tbxHeight";
+            this.tbxHeight.Size = new System.Drawing.Size(100, 20);
+            this.tbxHeight.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width";
+            // 
+            // tbxWidth
+            // 
+            this.tbxWidth.Location = new System.Drawing.Point(75, 39);
+            this.tbxWidth.Name = "tbxWidth";
+            this.tbxWidth.Size = new System.Drawing.Size(100, 20);
+            this.tbxWidth.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Area";
+            // 
+            // lblArea
+            // 
+            this.lblArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblArea.Location = new System.Drawing.Point(75, 69);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(100, 23);
+            this.lblArea.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Perimeter";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPerimeter.Location = new System.Drawing.Point(75, 99);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(100, 23);
+            this.lblPerimeter.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(75, 149);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 231);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxHeight);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rectangle Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Test 2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Test_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double height;
+            double width;
+
+            if (double.TryParse(tbxHeight.Text, out height) && double.TryParse(tbxWidth.Text, out width))
+            {
+                if (height > 0 && width > 0)
+                {
+                    double area = height * width;
+                    lblArea.Text = area.ToString();
+
+                    double perimeter = 2 * (height * width);
+                    lblPerimeter.Text = perimeter.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Positive Values Required", "Rectangle Calculator Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Numeric Values Required", "Rectangle Calculator Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
